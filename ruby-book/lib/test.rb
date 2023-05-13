a84a675d8ec2d8718d6d8d61d95ffc8bf54f944c
funxxfun <23shirasawa@gmail.com>
2023-05-13 13:23:21 +0900

    returnの練習 出力される値の比較

--- ruby-book/lib/test.rb
+++ ruby-book/lib/test.rb
@@ -9,12 +9,31 @@
 
 
 # returnの練習
+
+# その１
+# ⓪additionメソッドを定義する
+# def addition(num1, num2)
+#   result = num1 + num2
+
+# # ③処理して得られた結果resultを呼び出し元のadditionメソッドに返す
+#   return result 
+# end
+
+# # ①additionメソッドの引数に（1, 2）を渡して、result変数に代入する
+# result = addition(1, 2)
+# # ②addition関数に戻って処理を行う
+
+# # ④resultを出力する
+# puts result
+
+
+# その２
 # ⓪additionメソッドを定義する
 def addition(num1, num2)
   result = num1 + num2
 
 # ③処理して得られた結果resultを呼び出し元のadditionメソッドに返す
-  return result 
+  return "#{result}です。"
 end
 
 # ①additionメソッドの引数に（1, 2）を渡して、result変数に代入する
@@ -22,4 +41,4 @@ result = addition(1, 2)
 # ②addition関数に戻って処理を行う
 
 # ④resultを出力する
-puts result
+puts "合計は#{result}"
