e5aa4743db3eee7b9eef8bcc7f0cc46a6046f4fd
funxxfun <23shirasawa@gmail.com>
2023-05-13 13:50:43 +0900

    大吉2回でおみくじ終了する処理

--- ruby-book/lib/omikuji.rb
+++ ruby-book/lib/omikuji.rb
@@ -86,29 +86,28 @@ puts "おみくじを引いてください"
   
 
 # 大吉が出たらおみくじを終了する
-def omikuji(number)
-  selected_random_number = rand(1..number)
+# def omikuji(number)
+#   selected_random_number = rand(1..number)
   
-  if selected_random_number == 1
-    puts "大吉です"
-  elsif selected_random_number == 2
-    puts "中吉です"
-  elsif selected_random_number == 10
-    puts "凶です"
-  else
-    puts "小吉です"
-  end
-
-  return selected_random_number  # 結果を戻り値として返す
+#   if selected_random_number == 1
+#     puts "大吉です"
+#   elsif selected_random_number == 2
+#     puts "中吉です"
+#   elsif selected_random_number == 10
+#     puts "凶です"
+#   else
+#     puts "小吉です"
+#   end
   
-end
+# end
 
-result = 0
+# result = 0
 
-while result != 1 do
-  result = omikuji(20)
-  puts result
-end
+# # 無限ループになっているので注意！！
+# while result != 1 do
+#   result = omikuji(20)
+#   puts result
+# end
 
 
 
@@ -164,3 +163,60 @@ end
 #   result = omikuji(20)
 #   puts result
 # end
+
+
+# 大吉が出たらおみくじを終了する
+# 結果を戻り値として返すことで期待通りに動くようになった！！
+# def omikuji(number)
+#   selected_random_number = rand(1..number)
+  
+#   if selected_random_number == 1
+#     puts "大吉です"
+#   elsif selected_random_number == 2
+#     puts "中吉です"
+#   elsif selected_random_number == 10
+#     puts "凶です"
+#   else
+#     puts "小吉です"
+#   end
+
+#   return selected_random_number  # 結果を戻り値として返す
+  
+# end
+
+# result = 0
+
+# while result != 1 do
+#   result = omikuji(20)
+#   puts result
+# end
+
+
+#大吉が2回出たらおみくじを終了する
+def omikuji(number)
+    selected_random_number = rand(1..number)
+    
+    if selected_random_number == 1
+      puts "大吉です"
+    elsif selected_random_number == 2
+      puts "中吉です"
+    elsif selected_random_number == 10
+      puts "凶です"
+    else
+      puts "小吉です"
+    end
+  
+    return selected_random_number  # 結果を戻り値として返す
+    
+  end
+  
+  daikichi_counter = 0
+
+  while daikichi_counter <= 1 do
+    result = omikuji(20)
+    if result == 1
+     daikichi_counter += 1
+    end
+  end
+
+ 
