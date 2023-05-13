8d82fb5109ae06f004141f32bfa59188212b8a8e
funxxfun <23shirasawa@gmail.com>
2023-05-13 21:20:44 +0900

    .zero、.negative、.positiveを使用して比較する書き方

--- ruby-book/lib/test.rb
+++ ruby-book/lib/test.rb
@@ -44,20 +44,109 @@
 # puts "合計は#{result}"
 
 
+# # メソッド内のどこても使える戻り値
+# def hikaku(num1, num2)
+#   puts num1
+#   puts num2
+
+#   if num1 > num2
+#     return "大きい値は#{num1}です"
+#   elsif num1 < num2
+#     return  "大きい値は#{num2}です"
+#   else
+#     return "２つは同じ値です"
+#   end
+# end
+
+
+# result = hikaku(27, 5)
+# puts result
+
+# result = hikaku(10, 36)
+# puts result
+
+# result = hikaku(15, 15)
+# puts result
+
+
+
 # メソッド内のどこても使える戻り値
+# returnを1回のみ使用する書き方に変更
+# def hikaku(num1, num2)
+#   puts num1
+#   puts num2
+
+#   if num1 > num2
+#     num = num1
+#     puts "大きい数字は#{num}です。"
+#   elsif num1 < num2
+#     num = num2
+#     puts "大きい数字は#{num}です。"
+#   else
+#     num = num1 = num2
+#     puts "２つは同じ数字です。"
+#   end
+
+#   return num
+# end
+
+# result = hikaku(27, 5)
+# puts result
+
+# result = hikaku(10, 36)
+# puts result
+
+# result = hikaku(15, 15)
+# puts result
+
+
+
+# returnを1回のみ使用する書き方に変更
+# ChatGptの回答例
+# def hikaku(num1, num2)
+#   puts num1
+#   puts num2
+
+#   result =
+#     if num1 > num2
+#       "大きい値は#{num1}です"
+#     elsif num1 < num2
+#       "大きい値は#{num2}です"
+#     else
+#       "２つは同じ値です"
+#     end
+
+#   return result
+# end
+
+# result = hikaku(27, 5)
+# puts result
+
+# result = hikaku(10, 36)
+# puts result
+
+# result = hikaku(15, 15)
+# puts result
+
+
+
+# .zero、.negative、.positiveを使用した書き方
 def hikaku(num1, num2)
   puts num1
   puts num2
+  dif_num = num1 - num2
 
-  if num1 > num2
-    return "大きい値は#{num1}です"
-  elsif num1 < num2
-    return  "大きい値は#{num2}です"
-  else
-    return "２つは同じ値です"
+
+  if dif_num.positive?
+    puts "大きい数字は#{num1}です。"
+  elsif dif_num.negative?
+    puts "大きい数字は#{num2}です。"
+  elsif dif_num.zero?
+    puts "２つは同じ数字です。"
   end
-end
 
+  return dif_num
+end
 
 result = hikaku(27, 5)
 puts result
@@ -67,3 +156,5 @@ puts result
 
 result = hikaku(15, 15)
 puts result
+
+
