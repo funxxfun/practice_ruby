4aef4f447c1e2f7e0add508077e90e4cfe5dec24
funxxfun <23shirasawa@gmail.com>
2023-05-15 15:29:45 +0900

    じゃんけんの手数を変数に変更

--- ruby-book/lib/janken.rb
+++ ruby-book/lib/janken.rb
@@ -1,45 +1,47 @@
 puts "じゃんけんぽん"
 
 def janken
-  # 1: グー、2: チョキ、3: パーを定義する
+  # 0: グー、1: チョキ、2: パーを定義する
+
   gu = "グー"
   choki = "チョキ"
   pa = "パー"
 
-  puts "1:#{gu}  2:#{choki} 3:#{pa}"
+  gu_choki_pa = ["#{gu}", "#{choki}", "#{pa}"]
+  puts gu_choki_pa
 
   # 自分の手を選ぶ
-  # 1: グー、2: チョキ、3: パーの中から１つ選択する
+  # 0: グー、1: チョキ、2: パーの中から１つ選択する
   my_hand = gets.to_i
 
-  if my_hand == 1
-    my_hand = gu
-  elsif my_hand == 2
-    my_hand = choki
-  else
-    my_hand = pa
-  end
+  # if my_hand == 0
+  #   my_hand = gu
+  # elsif my_hand == 1
+  #   my_hand = choki
+  # else
+  #   my_hand = pa
+  # end
   
-  puts "あなたが出したのは#{my_hand}です。"
+  puts "あなたが出したのは#{gu_choki_pa[my_hand]}です。"
   
   # 相手の手を選ぶ
-  # 1: グー、2: チョキ、3: パーの中から無作為な値を生成する
-  opponent_hand = rand(1..3)
+  # 0: グー、1: チョキ、2: パーの中から無作為な値を生成する
+  opponent_hand = rand(2)
   
-  if opponent_hand == 1
-    opponent_hand = gu
-  elsif opponent_hand == 2
-    opponent_hand = choki
-  else
-    opponent_hand = pa
-  end
+  # if opponent_hand == 0
+  #   opponent_hand = gu
+  # elsif opponent_hand == 1
+  #   opponent_hand = choki
+  # else
+  #   opponent_hand = pa
+  # end
   
-  puts "相手が出したのは#{opponent_hand}です。"
+  puts "相手が出したのは#{gu_choki_pa[opponent_hand]}です。"
 
   if
-    (my_hand == gu) && (opponent_hand == choki) ||
-    (my_hand == choki) && (opponent_hand == pa) ||
-    (my_hand == pa) && (opponent_hand == gu)
+    (gu_choki_pa[my_hand] == gu ) && (gu_choki_pa[opponent_hand] == choki) ||
+    (gu_choki_pa[my_hand] == choki) && (gu_choki_pa[opponent_hand] == pa) ||
+    (gu_choki_pa[my_hand] == pa) && (gu_choki_pa[opponent_hand] == gu)
     puts "あなたの勝ちです"
   elsif my_hand == opponent_hand
     puts "あいこでしょ"
