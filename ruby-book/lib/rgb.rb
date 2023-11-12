eef5b3f25816bbc1473d69c2c4e2b2f6b09190a9
funxxfun <23shirasawa@gmail.com>
2023-11-12 16:18:06 +0900

    上級編のリファクタリングした部分を理解している範囲で書き換えた

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -5,19 +5,8 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
-  # r = hex[1..2]
-  # g = hex[3..4]
-  # b = hex[5..6]
-
-  # リファクタリングして1行にする
   r, g, b = hex[1..2], hex[3..4], hex[5..6]
-  
-  # [r, g, b].map do |s|
-  #   s.hex
-  
-  # [r, g, b].map { |s| s.hex }
-  
-  # リファクタリングして1行にする
-  [r, g, b].map(&:hex)
-
+  [r, g, b].map do |s|
+    s.hex
+  end
 end
